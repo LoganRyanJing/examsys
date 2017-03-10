4759580e0d01c50e74ebe8474ba04261587e39b0
logan <425068896@qq.com>
2017-03-10 12:01:03 +0800

    修改配置文件

--- lib/config.inc.php
+++ lib/config.inc.php
@@ -13,10 +13,10 @@ define('WP','http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']));
 else
 define('WP','http://'.$_SERVER['SERVER_NAME'].'/');
 
-define('DB','pe301');//MYSQL数据库名
+define('DB','pechina');//MYSQL数据库名
 define('DH','localhost');//MYSQL主机名，不用改
 define('DU','root');//MYSQL数据库用户名
-define('DP','');//MYSQL数据库用户密码
+define('DP','hansen');//MYSQL数据库用户密码
 define('DTH','x2_');//系统表前缀，不用改
 
 ?>
